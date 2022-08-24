831a88179f277de29c836e2397a288ae158b44d3
martypavalko <marty.pavalko@gmail.com>
2022-08-23 21:38:16 -0400

    Started to configure YouCompleteMe for GoLang

--- .config/nvim/lua/marty/plugins.lua
+++ .config/nvim/lua/marty/plugins.lua
@@ -2,5 +2,9 @@ return require('packer').startup(function(use)
 
 	use 'wbthomason/packer.nvim'
     use {'morhetz/gruvbox', as = 'gruvbox'}
+    use 'fatih/vim-go'
+    use 'vim-airline/vim-airline'
+    use 'preservim/nerdtree'
+    use 'ycm-core/YouCompleteMe'
 
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -78,6 +78,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/marty/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["vim-go"] = {
+    loaded = true,
+    path = "/home/marty/.local/share/nvim/site/pack/packer/start/vim-go",
+    url = "https://github.com/fatih/vim-go"
   }
 }
 
