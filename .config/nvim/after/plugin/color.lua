a339fad706ab407a3c97a7bc3c3bd02fab448c05
martypavalko <marty.pavalko@gmail.com>
2022-08-31 17:38:27 -0400

    Added scripts and setup shell script

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -1,3 +1,5 @@
+--vim.g.gruvbox_transparent = true
 vim.opt.background = "dark"
 
 vim.cmd("colorscheme gruvbox")
+vim.cmd("hi! Normal ctermbg=NONE guibg=NONE")
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -Syyu
+sudo pacman -S alacritty zsh i3-wm polybar rofi python3 yay git neovim
+cd ~/
+git clone git@github.com:martypavalko/Dotfiles.git
+ln -s /home/marty/.config/i3/config /home/marty/Dotfiles/.config/i3/config
+ln -s /home/marty/.config/alacritty/alacritty.yml /home/marty/Dotfiles/.config/alacritty/alacritty.yml
+ln -s /home/marty/.zshrc /home/marty/Dotfiles/.zshrc
