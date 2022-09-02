62d58dd87a56e03c93e7c20f6d1f6d7b7207ad80
martypavalko <marty.pavalko@gmail.com>
2022-09-01 21:30:40 -0400

    Rearranged setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syyu
 sudo pacman -S alacritty zsh i3-gaps polybar rofi python3 neovim ranger tmux npm firefox arandr
 cd ~/
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git config --global user.email "marty.pavalko@gmail.com"
 git config --global user.name "martypavalko"
 git clone --depth 1 "https://github.com/wbthomason/packer.nvim" "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
