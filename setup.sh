6e61b570346bbaef29c329fcf4742b36d2b98f46
martypavalko <marty.pavalko@gmail.com>
2022-09-01 21:58:53 -0400

    fixed pathing

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo pacman -Syyu
 sudo pacman -S alacritty zsh i3-gaps polybar rofi python3 neovim ranger tmux npm firefox arandr base-devel binutils make gcc fakeroot 
-cd ~/
+cd ~
 sudo git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
-cd ~/
+cd ~
 git config --global user.email "marty.pavalko@gmail.com"
 git config --global user.name "martypavalko"
 yay -Syu
