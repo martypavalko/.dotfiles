796e43dd1bed55c95ed8c7b013a3cb24a8be8c69
martypavalko <marty.pavalko@gmail.com>
2022-09-01 21:57:42 -0400

    More setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 sudo pacman -Syyu
-sudo pacman -S alacritty zsh i3-gaps polybar rofi python3 neovim ranger tmux npm firefox arandr
+sudo pacman -S alacritty zsh i3-gaps polybar rofi python3 neovim ranger tmux npm firefox arandr base-devel binutils make gcc fakeroot 
+cd ~/
+sudo git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
 cd ~/
 git config --global user.email "marty.pavalko@gmail.com"
 git config --global user.name "martypavalko"
-git clone --depth 1 "https://github.com/wbthomason/packer.nvim" "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
+yay -Syu
+yay -S nvim-packer-git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
