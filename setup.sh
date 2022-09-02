358f3b39bed447cee197ba9cbb62fd0a9f42cce7
martypavalko <marty.pavalko@gmail.com>
2022-09-01 22:03:39 -0400

    added chown

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo pacman -Syyu
 sudo pacman -S alacritty zsh i3-gaps polybar rofi python3 neovim ranger tmux npm firefox arandr base-devel binutils make gcc fakeroot 
 cd ~
 sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R marty:marty ./yay-git
 cd yay-git
 makepkg -si
 cd ~
