2b831466874835998ae96962c8b216aae54eaaea
martypavalko <marty.pavalko@gmail.com>
2022-12-19 17:02:50 -0500

    fixed leader

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,10 +3,10 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'sumneko_lua',
-	'rust_analyzer',
+	--'tsserver',
+	--'eslint',
+	--'sumneko_lua',
+	--'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
