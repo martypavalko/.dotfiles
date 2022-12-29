280cf9d4641f9a82fde6a1f55852081c2b160546
martypavalko <marty.pavalko@gmail.com>
2022-12-29 17:01:53 -0500

    tidying up

---
+++ old_nvim_lua/keymap/init.lua
@@ -0,0 +1,16 @@
+local Remap = require("marty.keymaps")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
+
+-- NERDTree
+nnoremap("<leader>b", ":NERDTreeToggle<CR>")
+
+-- Telescope
+nnoremap("<leader>ff", ":Telescope find_files hidden=true<CR>")
+nnoremap("<leader>fg", ":Telescope live_grep hidden=true<CR>")
+nnoremap("<leader>fb", ":Telescope buffers<CR>")
+nnoremap("<leader>fh", ":Telescope help_tags<CR>")
+nnoremap("<leader>ex", ":Ex<CR>")
