4b6a124f20a9ce69e4759c3c234da1baf66b183e
Marty Pavalko <martypavalko@gmail.com>
2023-11-05 13:18:49 -0500

    Change cmp keybinds for autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -101,9 +101,9 @@ cmp.setup({
   },
   formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
