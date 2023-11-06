983ecc357406665f3930fc6925ab44acabc01044
Marty Pavalko <martypavalko@gmail.com>
2023-11-05 22:50:04 -0500

    Adjusted keymappings for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 -- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+lopcal builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Search [F]iles" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Search by [G]rep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Search [B]uffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Search [H]elp" })
+vim.keymap.set('n', '<leader>fr', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true })<CR>', { desc = 'Search Hidden Files' })
