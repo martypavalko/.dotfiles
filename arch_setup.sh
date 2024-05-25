66e70781092dc88ad0e28fc6da9051d35ee9a56c
Marty Pavalko <marty@martypavalko.com>
2024-05-25 13:02:27 -0400

    beginning automating initial setup

---
+++ arch_setup.sh
@@ -0,0 +1,32 @@
+timedatectl set-timezone US/Eastern
+pacman -S sudo vim man-db man-pages texinfo networkmanager zsh
+useradd marty && usermod -aG wheel marty
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S hyprland
+pacman -S
+fakeroot debugedit make gcc cmake dunst pipewire polkit-kde-agent qt5-wayland qt6-wayland sddm xdg-desktop-portal-hyprland layer-shell-qt5 layer-shell-qt wayland xwayland alacritty wofi dolphin firefox wireplumber pipewire waybar hyprpaper ninja wayland-protocols libjpeg-turbo libwebp pango cairo pkgconf libglvnd wayland openssh fastfetch ttf-jetbrains-mono-nerd
+
+tee -a /etc/sddm.conf.d/10-wayland.conf <<EOF
+[General]
+DisplayServer=wayland
+GreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell
+
+[Wayland]
+CompositorCommand=kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1
+EOF
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git config --global user.name "Marty Pavalko"
+git config --global user.email marty@martypavalko.com
+
+mkdir -p /home/marty/.ssh && chmod 700 /home/marty/.ssh
+tee -a /home/marty/.ssh <<EOF
+Host github.com
+    HostName github.com
+    PreferredAuthentications publickey
+    IdentityFile /home/marty/.ssh/github
+EOF
+
+pacman -S starship fzf
+
