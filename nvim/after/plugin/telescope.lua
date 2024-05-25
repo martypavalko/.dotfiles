0fa7206228211c2448628cfc59a642ba29ffe94b
Marty Pavalko <marty@martypavalko.com>
2024-05-25 13:04:28 -0400

    hyprland overhaul

--- nvim/after/plugin/telescope.lua
+++
@@ -1,7 +0,0 @@
--- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Search [F]iles" })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Search by [G]rep" })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Search [B]uffers" })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Search [H]elp" })
-vim.keymap.set('n', '<leader>fr', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true })<CR>', { desc = 'Search Hidden Files' })
