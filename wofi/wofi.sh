6137d9998bf8a4d1dea90dcbe491c0d2cd88254b
Marty Pavalko <marty@martypavalko.com>
2024-05-28 00:15:19 -0400

    trying to make wofi a little cleaner

--- wofi/wofi.sh
+++ wofi/wofi.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-CONFIG="$HOME/.config/wofi/config/config"
+# CONFIG="$HOME/.config/wofi/config/config"
+CONFIG="$HOME/.config/wofi/cfg"
 STYLE="$HOME/.config/wofi/src/mocha/style.css"
+# STYLE="$HOME/.config/wofi/style.css"
 
 if [[ ! $(pidof wofi) ]]; then
     wofi --conf "${CONFIG}" --style "${STYLE}"
