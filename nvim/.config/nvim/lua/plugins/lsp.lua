9ef5990b123fd8bf7aaa622a31478ca31f1b701d
Marty Pavalko <git@martypavalko.com>
2026-09-24 23:34:19 -0400

    disable inlay hints by default

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,8 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ["*"] = {
-          keys = {
-            {
-              "K",
-              function()
-                return vim.lsp.buf.hover({ max_width = 80, max_height = 20 })
-              end,
-              desc = "Hover",
-            },
-          },
-        },
-      },
-    },
+  "neovim/nvim-lspconfig",
+  opts = {
+    inlay_hints = {
+      enabled = false,
+    }
   },
 }
